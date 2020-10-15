2f45e30f05400fd9ab356d7742b04e0976c11a16
Travis snɯǝᗡɔW <aoeu@users.noreply.github.com>
2020-10-15 14:26:27 -0400

    Add method for detecting if accessibility enabled for Service

--- java/homeward/bound/Main.java
+++ java/homeward/bound/Main.java
@@ -3,6 +3,7 @@ package homeward.bound;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class Main extends Activity {
 
@@ -20,5 +21,13 @@ public class Main extends Activity {
 	boolean shouldStopService() {
 		Bundle b = getIntent().getExtras();
 		return b != null && b.getBoolean(Service.keyToStop, false);
+	}
+
+	boolean isAccessibilityEnabled() {
+		String s = Settings.Secure.getString(
+			getApplicationContext().getContentResolver(),
+			Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+		);
+		return s != null && s.contains(getPackageName() +  "/" + Service.class.getName());
 	}
 }
